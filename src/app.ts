9c70af55456e7e8434a1ccf002a0a26df5cdee39
DanielFilipe100 <daniel.filipesoarescv@gmail.com>
2025-12-17 18:27:45 -0300

    correcoes para deploy

---
+++ src/app.ts
@@ -0,0 +1,26 @@
+import cors from "cors";
+import dotenv from 'dotenv';
+import express from 'express';
+import routes from "./Routes/router.ts";
+import cookieParser from 'cookie-parser';
+
+dotenv.config();
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors({
+    origin: process.env.ENVIRONMENT == "development" ? "http://localhost:3001" : process.env.APP_URL, 
+    credentials: true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api/v1/', routes);
+
+app.listen(port, () => {
+        console.log(`
+        ===================================================
+               Server listening on port ${port}
+        ===================================================
+        `)
+});
