34588c2b204ed6a8f6a71bb9a406174ed368ce69
DanielFilipe100 <daniel.filipesoarescv@gmail.com>
2025-12-18 21:20:11 -0300

    tentativa de corrijir o mailer

--- src/Lib/Mail/Mailer.ts
+++ src/Lib/Mail/Mailer.ts
@@ -1,16 +1,18 @@
-import { createTransport } from "nodemailer";
+import nodemailer from "nodemailer";
 
 class Mailer {
 
     private transporter;
 
     public constructor() {
-        this.transporter = createTransport({
-            service: process.env.MAIL_SERVER,
+        this.transporter = nodemailer.createTransport<any>({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            secure: false,
             auth: {
                 user: process.env.EMAIL_USER,
                 pass: process.env.EMAIL_PASS,
-            },
+            },  
         });
     }
 
